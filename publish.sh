f269c77ef36d8288157d54e959171578e4615679
Dénes Harmath <harmathdenes@gmail.com>
2014-11-12 00:42:50 +0100

    Make scripts more robust

--- publish.sh
+++ publish.sh
@@ -1,7 +1,12 @@
 set -e
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-if [[ $BRANCH -ne "master" ]] ; then
-    exit 1
+if [ "$BRANCH" != "master" ]; then
+  echo "Current branch has to be master!"
+  exit 1
+fi
+if [ -n "$(git status --porcelain)" ]; then 
+  echo "Working directory must be clean!"
+  exit 1
 fi
 git stash
 git checkout gh-pages
