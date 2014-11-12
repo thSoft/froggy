9d40688ebfc409aa384876d9ddd01f9246fdde97
Dénes Harmath <harmathdenes@gmail.com>
2014-11-13 00:53:12 +0100

    Transform touch events to mouse events in JavaScript

--- Froggy/Commands.elm
+++ Froggy/Commands.elm
@@ -2,7 +2,6 @@ module Froggy.Commands where
 
 import Keyboard
 import Mouse
-import Touch
 import Graphics.Input (..)
 import Froggy.Grid as Grid
 import Froggy.Model (..)
@@ -18,8 +17,7 @@ commands =
   let moveBy = lift2 makeMoveBy Keyboard.shift Keyboard.arrows
       continueWithKeyboard = lift makeContinue Keyboard.enter
       continueWithMouse = lift makeContinue Mouse.isDown
-      continueWithTouchscreen = lift makeContinue realTaps
-  in merges [moveBy, continueWithKeyboard, continueWithMouse, continueWithTouchscreen, moveTo.signal]
+  in merges [moveBy, continueWithKeyboard, continueWithMouse, moveTo.signal]
 
 makeMoveBy : Bool -> Grid.Position -> Command
 makeMoveBy shift arrows =
@@ -31,11 +29,6 @@ makeMoveBy shift arrows =
 
 makeContinue : Bool -> Command
 makeContinue pressed = if pressed then Continue else Nop
-
-realTaps : Signal Bool
-realTaps =
-  let isReal { x, y } = (x /= 0) && (y /= 0)
-  in lift isReal Touch.taps
 
 moveTo : Input Command
 moveTo = input Nop
